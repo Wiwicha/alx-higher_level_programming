a0e9be1bb8900ebf58b6e7b11c7c5e0f8a383e7e
wiam <wiam.silabdi.eln@gmail.com>
2023-11-28 14:26:25 -0800

    Task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i <= 9:
-        print('0{:d}'.format(i), end=', ' if i == 99 else '\n')
-    else:
+for i in range(0, 100)
+        print('0{:d}'.format(i), end=', 'if i <= 9)
         print('{:d}'.format(i), end=', ' if i == 99 else ' ')
